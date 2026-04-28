502d53896c9d9a698a345aad6c81b44b4f3e0a3d
Marcos Castro <profmarcoscastro.aee@gmail.com>
2026-04-28 01:22:24 -0300

    Incluindo sugestões de tecnologias no PAEE

--- app.py
+++ app.py
@@ -521,32 +521,32 @@ def gerar_pdf_paee(conteudo, codigo):
     ))
 
    for linha in conteudo.split("\n"):
-    linha = linha.strip()
+       linha = linha.strip()
 
-    # 🔥 remove título duplicado da IA
-    if "plano de atendimento educacional especializado" in linha.lower():
-        continue
+       # 🔥 remove título duplicado da IA
+       if "plano de atendimento educacional especializado" in linha.lower():
+           continue
 
-    if linha in ["--", "• --", "---"]:
-        continue
+       if linha in ["--", "• --", "---"]:
+           continue
 
-    if not linha:
-        elementos.append(Spacer(1, 6))
+       if not linha:
+           elementos.append(Spacer(1, 6))
 
-    elif linha.startswith("#"):
-        elementos.append(Paragraph(
-            f"<b>{linha.replace('#','').strip()}</b>",
-            secao_style
-        ))
+       elif linha.startswith("#"):
+           elementos.append(Paragraph(
+               f"<b>{linha.replace('#','').strip()}</b>",
+               secao_style
+           ))
 
-    elif linha.startswith("-"):
-        elementos.append(Paragraph(
-            f"• {linha[1:].strip()}",
-            normal_style
-        ))
+       elif linha.startswith("-"):
+            elementos.append(Paragraph(
+                f"• {linha[1:].strip()}",
+                normal_style
+           ))
 
-    else:
-        elementos.append(Paragraph(linha, normal_style))
+       else:
+            elementos.append(Paragraph(linha, normal_style))
 
     elementos.append(Spacer(1, 20))
     elementos.append(Paragraph(
